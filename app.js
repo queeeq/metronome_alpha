cb449325ff43ae47240f026e0c556843be1ab698
queeeq <quee@Maciek>
2024-09-02 19:36:54 +0200

    Set min and max tempos and activated the slider

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const tempoText = document.querySelector(".tempo-text");
 
 const increaseTempoButton = document.querySelector(".increase-tempo");
 const decreaseTempoButton = document.querySelector(".decrease-tempo");
-const tempoSlider = document.querySelector(".slider-thumb");
+const tempoSlider = document.querySelector(".slider");
 
 const startStopButton = document.querySelector(".start-stop");
 
@@ -13,16 +13,19 @@ const addBeatsButton = document.querySelector(".add-beats");
 const subtractBeatsButton = document.querySelector(".subtract-beats");
 const measuresDisplay = document.querySelector(".measure-count");
 
-let tempo = 120;
+let tempo = 150;
 let measures = 4;
 
-// event listeners section: +/- buttons
+// event listeners section
+
+// +/- BPM buttons
 increaseTempoButton.addEventListener("click", () => {
   if (tempo >= 280) {
     return;
   }
   tempo += 1;
   tempoDisplay.textContent = tempo;
+  tempoSlider.value = tempo;
   console.log(tempo);
 });
 
@@ -32,9 +35,18 @@ decreaseTempoButton.addEventListener("click", () => {
   }
   tempo -= 1;
   tempoDisplay.textContent = tempo;
+  tempoSlider.value = tempo;
   console.log(tempo);
 });
 
+// slider handling
+tempoSlider.addEventListener("input", () => {
+  tempo = +tempoSlider.value;
+  tempoDisplay.textContent = tempo;
+  tempoSlider.value = tempo;
+});
+
+// +/- measures buttons
 addBeatsButton.addEventListener("click", () => {
   measures += 1;
   measuresDisplay.textContent = measures;
@@ -46,5 +58,3 @@ subtractBeatsButton.addEventListener("click", () => {
   measuresDisplay.textContent = measures;
   console.log(measures);
 });
-
-// slider handling
